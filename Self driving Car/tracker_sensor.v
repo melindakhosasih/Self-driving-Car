16afc48c27adbe2f61086e151e8e22812457a072
melindakhosasih <69807833+melindakhosasih@users.noreply.github.com>
2021-12-27 01:30:34 +0800

    Update tracker_sensor.v

--- Self driving Car/tracker_sensor.v
+++ Self driving Car/tracker_sensor.v
@@ -1,30 +1,31 @@
 module tracker_sensor(clk, reset, left_track, right_track, mid_track, state);
-    input clk;
-    input reset;
-    input left_track, right_track, mid_track;
-    output reg [1:0] state;
+  input clk;
+  input reset;
+  input left_track, right_track, mid_track;
+  output reg [1:0] state;
 
-    /////////////////////////////////////////////////////////////////////////////////
-    always@(posedge clk, posedge reset)begin
-      if(reset)begin
-        state <= 2'b00;
+  // TODO: Receive three tracks and make your own policy.
+  // Hint: You can use output state to change your action.
+  always@(posedge clk, posedge reset) begin
+    if(reset) begin
+      state <= 2'b00; //  stop
+    end else begin
+      if(left_track == 1 && right_track == 1) begin
+        state <= 2'b11; //  go straight
+      end else if(left_track == 1 && mid_track == 1 && right_track == 0) begin
+        state <= 2'b10; //  turn right
+      end else if(left_track == 1 && mid_track == 0 && right_track == 0) begin
+        state <= 2'b10; //  turn right
+      end else if(left_track == 0 && mid_track == 1 && right_track == 1) begin
+        state <= 2'b01; //  turn left
+      end else if(left_track == 0 && mid_track == 1 && right_track == 0) begin
+        state <= 2'b11; //  go straight
+      end else if(left_track == 0 && mid_track == 0 && right_track == 1) begin
+        state <= 2'b01; //  turn left
       end else begin
-        if(left_track == 0 && mid_track == 1 && right_track == 0)begin
-          state <= 2'b11;
-        end else if(left_track == 1 && mid_track == 1 && right_track == 0)begin
-          state <= 2'b10;
-        end else if(left_track == 1 && mid_track == 0 && right_track == 0)begin
-          state <= 2'b10;
-        end else if(left_track == 0 && mid_track == 1 && right_track == 1)begin
-          state <= 2'b01;
-        end else if(left_track == 0 && mid_track == 0 && right_track == 1)begin
-          state <= 2'b01;
-        end else begin
-          state <= 2'b00;
-        end
+        state <= 2'b00; //  stop
       end
     end
-
-    /////////////////////////////////////////////////////////////////////////////////
+  end
 
 endmodule
