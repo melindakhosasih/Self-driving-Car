0c386beee96a91ed66296dd9de6c0e8a37dada76
hoop__ <73927244+Hlinm-qq@users.noreply.github.com>
2021-12-27 18:11:26 +0800

    0 to 1;

--- Self driving Car/tracker_sensor.v
+++ Self driving Car/tracker_sensor.v
@@ -1,4 +1,4 @@
-module tracker_sensor(clk, reset, left_track, right_track, mid_track, state);
+/*module tracker_sensor(clk, reset, left_track, right_track, mid_track, state);
   input clk;
   input reset;
   input left_track, right_track, mid_track;
@@ -27,5 +27,35 @@ module tracker_sensor(clk, reset, left_track, right_track, mid_track, state);
       end
     end
   end
+
+endmodule
+*/
+
+module tracker_sensor(clk, reset, left_track, right_track, mid_track, state);
+    input clk;
+    input reset;
+    input left_track, right_track, mid_track;
+    output reg [1:0] state;
+
+    always@(posedge clk, posedge reset)begin
+      if(reset)begin
+        state <= 2'b00;           // stop
+      end else begin
+        if(left_track == 1 && mid_track == 0 && right_track == 1)begin
+          state <= 2'b11;         // forward
+        end else if(left_track == 0 && mid_track == 0 && right_track == 1)begin
+          state <= 2'b10;         // left
+        end else if(left_track == 0 && mid_track == 1 && right_track == 1)begin
+          state <= 2'b10;         // left
+        end else if(left_track == 1 && mid_track == 0 && right_track == 0)begin
+          state <= 2'b01;         // right
+        end else if(left_track == 1 && mid_track == 1 && right_track == 0)begin
+          state <= 2'b01;         // right
+        end else begin
+          state <= 2'b00;         // stop
+        end
+      end
+    end
+
 
 endmodule
