0be4227653d50869909dd895d882c8b0725e46de
hoop__ <73927244+Hlinm-qq@users.noreply.github.com>
2021-12-28 18:30:09 +0800

    Update lab9_top.v

--- Self driving Car/lab9_top.v
+++ Self driving Car/lab9_top.v
@@ -12,12 +12,11 @@ module Lab9(
     output IN4,
     output left_pwm,
     output right_pwm
-    // You may modify or add more input/ouput yourself.
 );
     // We have connected the motor, tracker_sensor and sonic_top modules in the template file for you.
     // TODO: control the motors with the information you get from ultrasonic sensor and 3-way track sensor.
-
-    reg [1:0] mode;
+    
+    reg [1:0] mode, next_mode;
     wire [19:0] distance;
     wire [1:0] tracker_state;
     
@@ -47,16 +46,35 @@ module Lab9(
         .state(tracker_state)
     );
 
-    always@(posedge clk, posedge rst) begin
-        if(rst) begin
-            mode <= 2'b00;
-        end else begin
-            if(distance > 20) begin
-                mode <= tracker_state;
-            end else begin
-                mode <= 2'b00;
-            end
-        end
+    /////////////////////////////////////////////////////////////////////////////////
+    always@(posedge clk, posedge rst)begin
+      if(rst)begin
+        mode <= 2'b00;
+      end else begin
+        mode <= next_mode;
+      end
     end
+
+    always@(*)begin
+      if(distance > 5)begin
+        case (tracker_state)
+            2'b00 : begin
+                next_mode = 2'b00;
+            end
+            2'b01 : begin
+                next_mode = 2'b01;
+            end
+            2'b10 : begin
+                next_mode = 2'b10;
+            end
+            2'b11 : begin
+                next_mode = 2'b11;
+            end
+        endcase 
+      end else begin
+        next_mode = 2'b00;
+      end
+    end
+    /////////////////////////////////////////////////////////////////////////////////
 
 endmodule
